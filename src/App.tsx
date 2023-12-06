64340368a13628a7a0436169bf345001745abbb9
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-06 20:45:22 -0300

    Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 
 export default function App() {
-  return <h1>Bookkeeper!</h1>;
+  return (
+    <>
+     <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </>
+  );
 }

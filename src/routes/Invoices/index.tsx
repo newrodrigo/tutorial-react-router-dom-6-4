29553c9003d50ac73698cd2477c81d06f2667462
newrodrigo <rodrigocerqueira@outlook.com>
2023-12-12 21:45:12 -0300

    Reading URL Params

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { getInvoices } from "../../data";
 
 export default function Invoices() {
@@ -6,22 +6,23 @@ export default function Invoices() {
 
   return (
     <div style={{ display: "flex" }}>
-    <nav
-      style={{
-        borderRight: "solid 1px",
-        padding: "1rem",
-      }}
-    >
-      {invoices.map((invoice) => (
-        <Link
-          style={{ display: "block", margin: "1rem 0" }}
-          to={`/invoices/${invoice.number}`}
-          key={invoice.number}
-        >
-          {invoice.name}
-        </Link>
-      ))}
-    </nav>
-  </div>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        {invoices.map((invoice) => (
+          <Link
+            style={{ display: "block", margin: "1rem 0" }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </Link>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
   );
 }
